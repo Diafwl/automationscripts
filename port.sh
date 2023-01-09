7f68dcdde5458210dcb3425f251964a2383c86e9
Diafwl <90415547+Diafwl@users.noreply.github.com>
2023-01-09 21:49:50 +0300

    Update port.sh

--- port.sh
+++ port.sh
@@ -1,15 +1,20 @@
-HOST_PORT=${2}
-GUEST_IPADDR=${3}
-GUEST_PORT=${4}
+HOST_IPADDR=${2}
+HOST_PORT=${3}
+GUEST_IPADDR=${4}
+GUEST_PORT=${5}
+PROTOCOL=${6}
 
-if [ "${1}" = "add" ]; then
-	iptables -t nat -A PREROUTING -p tcp --dport "$HOST_PORT" -j DNAT --to "$GUEST_IPADDR:$GUEST_PORT"
-	iptables -I FORWARD -d "$GUEST_IPADDR/32" -p tcp -m state --state NEW -m tcp --dport "$GUEST_PORT" -j ACCEPT
-	echo ":${2} forwarded to ${3}:${4}"
+if [ -z $PROTOCOL ]; then
+        echo "_1:add/del,2:host_ip,3:host_port,4:guest_ip,5:guest_port,6:tcp/udp"
+
+elif [ "${1}" = "add" ]; then
+        iptables -t nat -A PREROUTING -d "$HOST_IPADDR/32" -p $PROTOCOL --dport "$HOST_PORT" -j DNAT --to "$GUEST_IPADDR:$GUEST_PORT"
+        iptables -I FORWARD -d "$GUEST_IPADDR/32" -p $PROTOCOL -m state --state NEW -m $PROTOCOL --dport "$GUEST_PORT" -j ACCEPT
+        echo "$2:${3} forwarded to ${4}:${5} ${6}"
 elif [ "${1}" = "del" ]; then
-        iptables -t nat -D PREROUTING -p tcp --dport "$HOST_PORT" -j DNAT --to "$GUEST_IPADDR:$GUEST_PORT"
-        iptables -D FORWARD -d "$GUEST_IPADDR/32" -p tcp -m state --state NEW -m tcp --dport "$GUEST_PORT" -j ACCEPT
-	echo "deleted :${2} forward to ${3}:${4}"
+        iptables -t nat -D PREROUTING -d "$HOST_IPADDR/32" -p $PROTOCOL --dport "$HOST_PORT" -j DNAT --to "$GUEST_IPADDR:$GUEST_PORT"
+        iptables -D FORWARD -d "$GUEST_IPADDR/32" -p $PROTOCOL -m state --state NEW -m $PROTOCOL --dport "$GUEST_PORT" -j ACCEPT
+        echo "deleted :${2} forward to ${3}:${4}"
 else
-	echo "1:add/del,2:host_port,3:guest_ip,4:guest_port"
+    	echo "1:add/del,2:host_ip,3:host_port,4:guest_ip,5:guest_port,6:tcp/udp"
 fi
